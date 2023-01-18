325eff4255fc379b7f94c20b0698fb8c8fc8048a
Makayla <makaylabusher@gmail.com>
2023-01-18 14:30:55 -0600

    hardcode data in content(for index)

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,14 @@
+import { ProductsIndex } from "./ProductsIndex";
+
 export function Content() {
+  const products = [
+    {id: 1, name: "book", price: 15.00, description: "a book you can read", inventory: 5},
+    {id: 2, name: "shirt", price: 15.00, description: "a shirt you can wear", inventory: 10}
+  ]
+
   return (
     <div>
-      <h1>Hello from Content</h1>
+      <ProductsIndex products={products}/>
     </div>
   );
 }
