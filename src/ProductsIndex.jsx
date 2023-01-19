bdcf7c755a793fe8d92a7a9a7341bf51fb803484
Makayla <makaylabusher@gmail.com>
2023-01-19 13:10:11 -0600

    add images to index

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -3,11 +3,16 @@ export function ProductsIndex(props) {
     <div>
       <h1>All Products</h1>
       {props.products.map((product) => (
-        <div key={product.id}> 
+        <div className="products" key={product.id}> 
           <h2>{product.name}</h2>
           <p>Price: {product.price}</p>
           <p>Description: {product.description}</p>
           <p>Inventory: {product.inventory}</p>
+          {product['images'].map(image => (
+            <div> 
+              <img src={image.url} />
+            </div>
+          ))}
         </div>
       ))}
     </div>
